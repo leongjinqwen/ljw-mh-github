e17835b1ff6f058889d8a0562ef3d6882ed7bb62
leongjinqwen <leongjinqwen@hotmail.com>
2020-07-30 11:30:33 +0800

    amend code

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def greet():
     print("Hello!!!!")
+    print("123123234534!!!!")
+    print("asdasdasdaksjdhkjad!!!!")
+    print("Hello!!!!")
     print("World!!!!")
-    print("Hello!!!!")
-    print("Hello!!!!")
